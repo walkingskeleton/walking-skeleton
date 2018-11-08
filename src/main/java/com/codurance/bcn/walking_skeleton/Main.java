a0d531c782ad9f6b2e1d144d936759888b224649
David Gracia <david.gracia.celemendi@gmail.com>
2018-11-08 18:34:18 +0100

    changes

--- src/main/java/com/codurance/bcn/walking_skeleton/Main.java
+++
@@ -1,9 +0,0 @@
-package com.codurance.bcn.walking_skeleton;
-
-import java.io.IOException;
-
-public class Main {
-    public static void main(String... args) throws IOException {
-        new Console().read();
-    }
-}
